0b3bf456ed52aab92fafdbbf2191af10eda46edd
Suhadak Akbar <ggsuha@gmail.com>
2023-01-31 21:28:59 +0800

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: '"Inter var"',
+        ...defaultTheme.fontFamily.sans,
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};

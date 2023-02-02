6d0f6250ed7932adffdf021657bbaadb41d8cb68
Suhadak Akbar <ggsuha@gmail.com>
2023-02-02 09:34:53 +0800

    useFetch and useAsyncData

--- app.vue
+++ app.vue
@@ -1,25 +1,78 @@
+<script setup lang="ts">
+const search = ref("Toronto");
+const input = ref("");
+const background = ref("");
+
+const { data, error } = useAsyncData(
+  async () => {
+    let response;
+    try {
+      response = await $fetch(
+        `http://api.weatherapi.com/v1/current.json?key=db7df8852e3642f9a9e10323230202&q=${search.value}`);
+
+      const temp = response.current.temp_c;
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (e) { }
+    return response;
+  },
+  {
+    watch: [search],
+  }
+);
+
+const handleClick = () => {
+  const formatedSearch = input.value.trim().split(" ").join("+");
+  search.value = formatedSearch;
+  input.value = "";
+};
+</script>
+
 <template>
-  <div>
-    <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-      <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt=""
-        width="384" height="512">
-      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium">
-            “Tailwind CSS is the only framework that I've seen scale
-            on large teams. It’s easy to customize, adapts to any design,
-            and the build size is tiny.”
+  <div class="h-screen relative overflow-hidden">
+    <img :src="background" />
+    <div class="absolute w-full h-full top-0 overlay" />
+    <div class="absolute w-full h-full top-0 p-48">
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">{{ data.location.name }}</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <img :src="data.current.condition.icon" class="w-56 icon" />
+        </div>
+        <div>
+          <p class="text-9xl text-white font-extralight">
+            {{ data.current.temp_c }}°
           </p>
-        </blockquote>
-        <figcaption class="font-medium">
-          <div class="text-sky-500 dark:text-sky-400">
-            Sarah Dayan
-          </div>
-          <div class="text-slate-700 dark:text-slate-500">
-            Staff Engineer, Algolia
-          </div>
-        </figcaption>
+        </div>
       </div>
-    </figure>
+      <div class="mt-20">
+        <input type="text" class="w-1/2 h-10" placeholder="Search a data.city..." v-model="input" />
+        <button class="bg-sky-400 w-20 text-white h-10" @click="handleClick">
+          Search
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.icon {
+  margin-left: -2.75rem;
+  margin-top: -2.75rem;
+}
+</style>
